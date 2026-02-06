2bb4935aaffc1dbea0c3a119700d4e7d37a63dc3
sfan5 <sfan5@live.de>
2026-02-06 13:00:04 +0100

    Reduce array_texture_max to 1000 (hotfix)
    In #16896 we have two user reports that the maximum values
    before textures get jumbled are 2900 (GLES on phone) or 5440 (GL3 on desktop).
    With some extra safety margin let's pick 1000.
    
    This should fix the game-breaking bug for affected people,
    but still allow us to discover if array textures are fundamentally broken
    in case further bug reports come in.

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -265,7 +265,7 @@ void set_default_settings()
 	settings->setDefault("connected_glass", "false");
 	settings->setDefault("smooth_lighting", "true");
 	settings->setDefault("performance_tradeoffs", "false");
-	settings->setDefault("array_texture_max", "65535");
+	settings->setDefault("array_texture_max", "1000");
 	settings->setDefault("lighting_alpha", "0.0");
 	settings->setDefault("lighting_beta", "1.5");
 	settings->setDefault("display_gamma", "1.0");
