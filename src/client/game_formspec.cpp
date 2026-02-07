80b0cd47c50bfc6547f219434030dc2302c43101
vopidev <services@vopi.team>
2026-02-07 14:22:26 +0200

    Update pause menu design for VOPI Engine

--- src/client/game_formspec.cpp
+++ src/client/game_formspec.cpp
@@ -357,6 +357,39 @@ void GameFormSpec::showPlayerInventory(const std::string *fs_override)
 
 void GameFormSpec::showPauseMenu()
 {
+	std::ostringstream os;
+	auto simple_singleplayer_mode = m_client->m_simple_singleplayer_mode;
+
+#if IS_VOPI_ENGINE
+	float font_size = g_settings->getFloat("font_size");
+
+	const std::string background_image = "gui_common/gui_small_notebook_bg.png";
+	const std::string green_button_image = "gui_common/gui_big_green_btn.png";
+	const std::string red_button_image = "gui_common/gui_big_red_btn.png";
+	const std::string blue_button_image = "gui_common/gui_big_blue_btn.png";
+	const std::string cat_image = "gui_common/gui_pause_menu_cat.png";
+
+	os  << "formspec_version[9]" << "size[6,3;false]"
+		<< "no_prepend[]"
+		<< "real_coordinates[true]" << "bgcolor[#00000060;true]"
+		<< "background[0,0;0,0;" << background_image << ";true]"
+		<< "image[2.75,1.1;2.6,1.65;" << cat_image << "]"
+		<< "style_type[label;font_size=" << font_size * 1.5f << ";textcolor=#FF8BB2;font=bold]"
+		<< "label[3,0.85;" << strgettext("GAME PAUSED") << ";center]";
+	if (simple_singleplayer_mode) {
+		os	<< "style[btn_continue;bgcolor=#00000000;border=false;font_size=" << font_size * 0.95f << "]"
+			<< "style[btn_exit_menu;bgcolor=#00000000;border=false;font_size=" << font_size * 0.91f << "]"
+			<< "image_button_exit[0.5,1.15;1.63,0.61;" << green_button_image << ";btn_continue;" << strgettext("CONTINUE") << "]"
+			<< "image_button_exit[0.5,1.9;1.63,0.61;" << red_button_image << ";btn_exit_menu;" << strgettext("EXIT TO MENU") << "]";
+	} else {
+		os	<< "style[btn_continue;bgcolor=#00000000;border=false;font_size=" << font_size * 0.95f << "]"
+			<< "style[btn_change_password;bgcolor=#00000000;border=false;font_size=" << font_size * 0.93f << "]"
+			<< "style[btn_exit_menu;bgcolor=#00000000;border=false;font_size=" << font_size * 0.91f << "]"
+			<< "image_button_exit[0.5,1.05;1.63,0.53;" << green_button_image << ";btn_continue;" << strgettext("CONTINUE") << "]"
+			<< "image_button[0.5,1.62;1.63,0.53;" << blue_button_image << ";btn_change_password;" << strgettext("PASSWORD") << "]"
+			<< "image_button_exit[0.5,2.2;1.63,0.53;" << red_button_image << ";btn_exit_menu;" << strgettext("EXIT TO MENU") << "]";
+	}
+#else
 	std::string control_text;
 
 	if (g_touchcontrols) {
@@ -375,10 +408,7 @@ void GameFormSpec::showPauseMenu()
 			);
 	}
 
-	auto simple_singleplayer_mode = m_client->m_simple_singleplayer_mode;
-
 	float ypos = simple_singleplayer_mode ? 0.7f : 0.1f;
-	std::ostringstream os;
 
 	os << "formspec_version[1]" << SIZE_TAG
 		<< "button_exit[4," << (ypos++) << ";3,0.5;btn_continue;"
@@ -447,6 +477,7 @@ void GameFormSpec::showPauseMenu()
 		}
 	}
 	os << ";]";
+#endif
 
 	/* Create menu */
 	/* Note: FormspecFormSource and LocalFormspecHandler  *
