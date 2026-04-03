9f601869b5877dd359b37a2f043534a854fbdc00
vopidev <services@vopi.team>
2026-04-03 13:18:24 +0300

    Disable entity selection box by default

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -302,7 +302,7 @@ void set_default_settings()
 	settings->setDefault("smooth_scrolling", "true");
 	settings->setDefault("hud_hotbar_max_width", "1.0");
 	settings->setDefault("enable_local_map_saving", "false");
-	settings->setDefault("show_entity_selectionbox", "true"); //debug
+	settings->setDefault("show_entity_selectionbox", "false");
 	settings->setDefault("ambient_occlusion_gamma", "1.8");
 	settings->setDefault("arm_inertia", "true");
 	settings->setDefault("show_nametag_backgrounds", "true");
